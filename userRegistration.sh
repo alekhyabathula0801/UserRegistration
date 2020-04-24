8ac2917bb9a905620f7153cc4a61bf23bf4956db
arun kumar <alekhya0801@gmail.com>
2020-04-24 23:24:16 +0530

    Added feature to check email id

--- userRegistration.sh
+++ userRegistration.sh
@@ -22,9 +22,21 @@ lastName(){
 		lastName
         fi
 }
+email(){
+	emailPat='^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$'
+	read -p "Enter Email Id " email
+	if [[ $email =~ $emailPat ]]
+	then
+		echo Valid Email Id
+	else
+		echo Invalid Email Id
+		email
+	fi
+}
 userRegistration(){
 	namePat='^[A-Z][a-z]{2,}$'
 	firstName
 	lastName
+	email
 }
 userRegistration

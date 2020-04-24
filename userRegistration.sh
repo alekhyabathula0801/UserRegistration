ff5ab1f99b9afe8c9ff98d2855f13986d4649ec4
arun kumar <alekhya0801@gmail.com>
2020-04-24 22:39:36 +0530

    Added feature to check last name

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,9 +1,30 @@
 #!/bin/bash -x
-read -p "Enter first name " firstName
-firstNamePat='^[A-Z]+[a-z]{2,}$'
-if [[ $firstName =~ $firstNamePat ]]
-then
-	echo Yes
-else
-	echo No
-fi
+shopt -s extglob
+firstName(){
+	echo First name must start with capital letter and minimum 3 characters are requried
+	read -p "Enter first name " firstName
+	if [[ $firstName =~ $namePat ]]
+	then
+		echo Valid first name
+	else
+		echo Invalid first name
+		firstName
+	fi
+}
+lastName(){
+        echo Last name must start with capital letter and minimum 3 characters are requried
+        read -p "Enter last name " lastName
+        if [[ $lastName =~ $namePat ]]
+        then
+                echo Valid last name
+        else
+                echo Invalid last name
+		lastName
+        fi
+}
+userRegistration(){
+	namePat='^[A-Z][a-z]{2,}$'
+	firstName
+	lastName
+}
+userRegistration
